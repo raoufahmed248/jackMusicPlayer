65aee07b38d6c654210503d261bdd23bc5b612e4
Mohammed Ahmed <raouf.ahmed248@gmail.com>
2021-12-06 19:17:18 -0800

    WIP of decode function, compile failing

--- inc/wavPlayer.h
+++ inc/wavPlayer.h
@@ -11,28 +11,42 @@ public:
     int returnFour();
     wavPlayer(std::string wavFilePath, int queueSize, int delayBetweenPlays);
     ~wavPlayer();
+    // Begin parsing frames until queueSize is reached, returns frames parsed
     int decode();
+    // Parse only input amount of frames, returns frames parsed
     int decode(int amount);
+    // Get current amount of frames in queue
     int currentQueueSize();
+    // Get value of current sample index
     int currentSampleIndex();
+    // Get value of total number of sampels in wav file
     int totalSamples();
 
+    // Reset sample index
     void resetIndex();
 
+    //Retrieve samples from left and right channels.
+    //NOTE:THIS FUNCTION NEEDS TO ENSURE THERE ARE SAMPLES FOR BOTH LEFT AND RIGHT,OTHERWISE SEND 0's
+    int getLeftRightSamples(void *leftBuffer, void *rightBuffer, int amount);
     int getLeftFrame(void *buffer, int amount);
     int getRightFrame(void *buffer, int amount);
 
 private:
-    moodycamel::ConcurrentQueue<int[512]> *leftSampleQueue;
-    moodycamel::ConcurrentQueue<int[512]> *rightSampleQueue;    
-    int leftChannelReserveBuffer[512] = {};
-    int rightChannelReserveBuffer[512] = {};
+    static constexpr  size_t samplesPerFrame = 512;
+    moodycamel::ConcurrentQueue<float[512]> *leftSampleQueue;
+    moodycamel::ConcurrentQueue<float[512]> *rightSampleQueue;
+    float leftChannelReserveBuffer[512] = {};
+    float rightChannelReserveBuffer[512] = {};
+    float preParseBuffer[512*2] = {};
+    float scratchBuffer[512] = {};
     int leftChannelReserveAmount = {};
     int rightChannelReserveAmount = {};
     int currentIndex = {};
     drwav wav;
     std::string filePath = {};
 
-
+    size_t replayFrameDelay = {};
+    size_t currFrameDelay = {};
+    size_t queueSize = {};
 };
 #endif  
--- src/wavPlayer.cpp
+++ src/wavPlayer.cpp
@@ -11,9 +11,15 @@ wavPlayer::wavPlayer(std::string wavFilePath, int queueSize, int sampleDelayBetw
         // Error opening WAV file.
         throw std::invalid_argument("DR_WAV COULD NOT OPEN WAV FILE!");
     }
+    if(wav.channels > 2 || wav.channels < 1)
+    {
+        throw std::invalid_argument("THIS SOFTWARE ONLY SUPPORTS STEREO OR MONO WAV FILES (<=2 CHANNELS)!");
+    }
     filePath = wavFilePath;
-    leftSampleQueue = new moodycamel::ConcurrentQueue<int[512]> (queueSize);
-    rightSampleQueue = new moodycamel::ConcurrentQueue<int[512]> (queueSize);
+    this->queueSize = queueSize;
+    replayFrameDelay = sampleDelayBetweenPlays;
+    leftSampleQueue = new moodycamel::ConcurrentQueue<float[512]> (this->queueSize);
+    rightSampleQueue = new moodycamel::ConcurrentQueue<float[512]> (this->queueSize);
     
 
 
@@ -24,4 +30,81 @@ wavPlayer::~wavPlayer()
     drwav_uninit(&wav);
     delete leftSampleQueue;
     delete rightSampleQueue;
+}
+
+int wavPlayer::decode()
+{
+    size_t numOfEmptySpaces = queueSize - leftSampleQueue->size_approx();
+
+    size_t samplesToParse = {};
+    size_t totalDecoded = {};
+    if(wav.channels == 2)
+    {
+        samplesToParse = samplesPerFrame * 2;
+    }
+    else
+    {
+        samplesToParse = samplesPerFrame;
+    }
+
+    for(int x = 0; x < numOfEmptySpaces; x++)
+    {
+        if(currFrameDelay > 0)
+        {
+            memset(&preParseBuffer, 0 , samplesToParse * sizeof(preParseBuffer[0]));
+            currFrameDelay--;
+        } 
+        else
+        {
+            size_t numOfSamplesDecoded = drwav_read_pcm_frames_f32(&wav,
+                samplesToParse, (float *)preParseBuffer);
+            if(numOfSamplesDecoded < (samplesToParse))
+            {
+                size_t remaining_samples = (samplesToParse) - numOfSamplesDecoded;
+                memset(&preParseBuffer[numOfSamplesDecoded],0,remaining_samples * sizeof(preParseBuffer[0]));
+                drwav_seek_to_pcm_frame(&wav, 0);
+                currFrameDelay = replayFrameDelay;
+            }
+        }
+
+        if(wav.channels == 2)
+        {
+            //Parse left channel
+            for(int y = 0; y < samplesToParse; y+=2)
+            {
+                scratchBuffer[y] = preParseBuffer[y];
+            }
+            //technically we should throw exception if this fails...
+            if(leftSampleQueue->try_enqueue(scratchBuffer))
+            {
+                totalDecoded++;
+            }
+            //Parse right channel
+            for(int y = 1; y < samplesToParse; y+=2)
+            {
+                scratchBuffer[y] = preParseBuffer[y];
+            }
+            //technically we should throw exception if this fails...
+            if(rightSampleQueue->try_enqueue(scratchBuffer))
+            {
+                totalDecoded++;
+            }
+            
+        }
+        else
+        {
+            memcpy(scratchBuffer, preParseBuffer, samplesToParse * sizeof(preParseBuffer[0]));
+            if(leftSampleQueue->try_enqueue(scratchBuffer))
+            {
+                totalDecoded++;
+            }
+
+            if(rightSampleQueue->try_enqueue(scratchBuffer))
+            {
+                totalDecoded++;
+            }
+            
+        }
+    }
+    return totalDecoded;
 }
